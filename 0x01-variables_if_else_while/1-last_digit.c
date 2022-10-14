41765c45007b9a51725307153c03a510387e59ef
Chinonso Onaga <76971567+chinonsoScelloo@users.noreply.github.com>
2022-10-14 23:31:26 +0100

    update

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,33 +5,23 @@
  */
 int main(void)
 {
-	int i, k = '0', j;
-
-	for (i = '0'; i <= '9'; i++)
+	int n;
+	int l;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	l = n % 10
+	/* your code goes there */
+	if (l > 5)
 	{
-		for (j = k; j <= '9'; j++)
-		{
-			if (i != j)
-			{
-				putchar(i);
-				putchar(j);
-			}
-			if (i == j)
-			{
-				continue;
-			}
-			if (i == '8' && j == '9')
-			{
-				break;
-			}
-			else
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-		k++;
+		printf("Last digit of %d is %d and is greater than 5\n", n, l);
 	}
-	putchar('\n');
+	else if ( l == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, l);
+	}
+	else 
+	{
+		printf("Last digir of %d is %d and is less than 6 and not 0\n", n, l);
+	
 	return (0);
 }
