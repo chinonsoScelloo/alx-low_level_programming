f227e42735278d6fc620c67029d1424912d5b417
chinonsoScelloo <christian.onaga@gmail.com>
2022-10-13 17:00:21 -0700

    my first c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main-entry
+*
+* Return:0 if no errors, return non zero if errors
+*/
+int main(void)
+{
+printf("with proper grammer,but the outcome is piece of art,\n");
+return (0);
+}
+
+
