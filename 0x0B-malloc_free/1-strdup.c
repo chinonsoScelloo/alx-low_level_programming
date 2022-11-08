f3c0616fdeb9a9fab3df046839eaafa6f40515f7
Chinonso Onaga <76971567+chinonsoScelloo@users.noreply.github.com>
2022-11-09 00:17:46 +0100

    update

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,22 +13,15 @@ char *_strdup(char *str)
 {
 	char *duplicate;
 	int index, len = 0;
-
 	if (str == NULL)
-		return (NULL);
-
+	return (NULL);
 	for (index = 0; str[index]; index++)
-		len++;
-
+	len++;
 	duplicate = malloc(sizeof(char) * (len + 1));
-
 	if (duplicate == NULL)
-		return (NULL);
-
+	return (NULL);
 	for (index = 0; str[index]; index++)
-		duplicate[index] = str[index];
-
+	duplicate[index] = str[index];
 	duplicate[len] = '\0';
-
 	return (duplicate);
 }
