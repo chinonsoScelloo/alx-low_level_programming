0a3c64be4a0dd964c3faf1d5c922d9c4d1aef901
Chinonso Onaga <76971567+chinonsoScelloo@users.noreply.github.com>
2022-11-09 00:22:22 +0100

    update

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,17 +14,12 @@ char *create_array(unsigned int size, char c)
 {
   char *array;
   unsigned int index;
-
   if (size == 0)
-    return (NULL);
-
+  return (NULL);
   array = malloc(sizeof(char) * size);
-
   if (array == NULL)
-    return (NULL);
-
+  return (NULL);
   for (index = 0; index < size; index++)
-    array[index] = c;
-
+  array[index] = c;
   return (array);
 }
