bb4148afdda9fcf8e52b6269a28b55cfb489ff33
Chinonso Onaga <76971567+chinonsoScelloo@users.noreply.github.com>
2022-11-11 22:03:56 +0100

    update

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
  * _calloc - Allocates memory for an array of a certain number
  *           of elements each of an inputted byte size.
@@ -11,16 +12,22 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-void *mem;
-char *filler;
-unsigned int index;
-if (nmemb == 0 || size == 0)
-  return (NULL);
-  mem = malloc(size * nmemb);
-  if (mem == NULL)
-    return (NULL);
-  filler = mem;
-  for (index = 0; index < (size * nmemb); index++)
-    filler[index] = '\0';
-  return (mem);
+	void *mem;
+	char *filler;
+	unsigned int index;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	mem = malloc(size * nmemb);
+
+	if (mem == NULL)
+		return (NULL);
+
+	filler = mem;
+
+	for (index = 0; index < (size * nmemb); index++)
+		filler[index] = '\0';
+
+	return (mem);
 }
