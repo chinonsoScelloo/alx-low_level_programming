11058281d0fda08a5026336e7b7f68f92ebbe425
Chinonso Onaga <76971567+chinonsoScelloo@users.noreply.github.com>
2022-11-15 00:27:13 +0100

    update

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Prints the name of the file of the program
  *        was compiled from, followed by a new line.
@@ -9,6 +8,5 @@
 int main(void)
 {
   printf("%s\n", __FILE__);
-
   return (0);
 }
