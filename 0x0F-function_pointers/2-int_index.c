620b857a4d6804b69009aa2a83b3bad83119a6d2
Chinonso Onaga <76971567+chinonsoScelloo@users.noreply.github.com>
2022-11-17 18:19:03 +0100

    update

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-
 /**
  * int_index - Searches for an integer in an array of integers.
  * @array: The array of integers.
@@ -12,16 +11,13 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-  int index;
-
-  if (array == NULL || cmp == NULL)
-    return (-1);
-
-  for (index = 0; index < size; index++)
-    {
-      if (cmp(array[index]) != 0)
-	return (index);
-    }
-
-  return (-1);
+int index;
+if (array == NULL || cmp == NULL)
+return (-1);
+for (index = 0; index < size; index++)
+{
+if (cmp(array[index]) != 0)
+return (index);
+}
+return (-1);
 }
