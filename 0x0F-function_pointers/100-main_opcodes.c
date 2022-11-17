366581af45379614c359e798b6767a1bbbda7db9
Chinonso Onaga <76971567+chinonsoScelloo@users.noreply.github.com>
2022-11-18 00:18:14 +0100

    update

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * main - Prints the opcodes of itself.
  * @argc: The number of arguments supplied to the program.
@@ -10,37 +9,29 @@
  */
 int main(int argc, char *argv[])
 {
-	int bytes, index;
-	int (*address)(int, char **) = main;
-	unsigned char opcode;
-
-	if (argc != 2)
-	{
-		printf("Error\n");
-		exit(1);
-	}
-
-	bytes = atoi(argv[1]);
-
-	if (bytes < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
-
-	for (index = 0; index < bytes; index++)
-	{
-		opcode = *(unsigned char *)address;
-		printf("%.2x", opcode);
-
-		if (index == bytes - 1)
-			continue;
-		printf(" ");
-
-		address++;
-	}
-
-	printf("\n");
-
-	return (0);
+int bytes, index;
+int (*address)(int, char **) = main;
+unsigned char opcode;
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
+}
+bytes = atoi(argv[1]);
+if (bytes < 0)
+{
+printf("Error\n");
+exit(2);
+}
+for (index = 0; index < bytes; index++)
+{
+opcode = *(unsigned char *)address;
+printf("%.2x", opcode);
+if (index == bytes - 1)
+continue;
+printf(" ");
+address++;
+}
+printf("\n");
+return (0);
 }
